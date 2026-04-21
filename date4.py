2f92e361c98788daf98c11d08702d716a489f8e7
trithien119 <huynhtrithien119@gmail.com>
2026-04-21 11:26:14 +0700

    Delete date4.py

--- date4.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-
-now = datetime.now()
-# Thêm 5 giây
-new_time = now + timedelta(seconds=5)
-
-print(f"\nThời gian hiện tại: {now.strftime('%H:%M:%S')}")
-print(f"Thời gian sau khi thêm 5 giây: {new_time.strftime('%H:%M:%S')}")
